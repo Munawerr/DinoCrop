5d69face15d1e5d1de5530edf12fb01d9ef90357
Munawer Anwar <44364493+Munawerr@users.noreply.github.com>
2020-10-20 18:33:10 +0500

    dummy-data.js

---
+++ Restaurant.js
@@ -0,0 +1,11 @@
+class Restaurant {
+  constructor(id, title, coverimg, des, rating) {
+    this.id = id;
+    this.title = title;
+    this.coverimg = coverimg;
+    this.des = des;
+    this.rating = rating;
+  }
+}
+
+export default Restaurant;
---
+++ dummy-data.js
@@ -0,0 +1,76 @@
+import Restaurant from "../modals/Restaurant";
+
+const RESTAURANTS = [
+  new Restaurant(
+    "r1",
+    "Good Food",
+    "https://cdn.pixabay.com/photo/2016/12/26/17/28/food-1932466_960_720.jpg",
+    "Everything is awesome",
+    "4"
+  ),
+  new Restaurant(
+    "r2",
+    "Steak House",
+    "https://cdn.pixabay.com/photo/2014/11/05/15/57/salmon-518032_960_720.jpg",
+    "hum yahn pe Murgi k tike nahi bechte!!!",
+    "3"
+  ),
+  new Restaurant(
+    "r3",
+    "Bunny's Pancake",
+    "https://cdn.pixabay.com/photo/2017/05/07/08/56/pancakes-2291908_960_720.jpg",
+    "Made with love",
+    "4"
+  ),
+  new Restaurant(
+    "r4",
+    "Muffins Muffins Muffins",
+    "https://cdn.pixabay.com/photo/2015/03/26/09/39/cupcakes-690040_960_720.jpg",
+    "Its Muffin time, I just wanna die, die, die",
+    "2"
+  ),
+  new Restaurant(
+    "r5",
+    "The Night Dinner",
+    "https://cdn.pixabay.com/photo/2014/06/16/23/10/spice-370114_960_720.jpg",
+    "Dinner is ready",
+    "1"
+  ),
+  new Restaurant(
+    "r6",
+    "Veg Point",
+    "https://cdn.pixabay.com/photo/2017/09/22/19/05/casserole-dish-2776735_960_720.jpg",
+    "We're killing nature",
+    "5"
+  ),
+  new Restaurant(
+    "r7",
+    "Pasta Palace",
+    "https://cdn.pixabay.com/photo/2017/11/08/22/18/spaghetti-2931846_960_720.jpg",
+    "Gourme Italian class Pasta. Eat once and just lick your fingers & leave",
+    "4"
+  ),
+  new Restaurant(
+    "r8",
+    "Papa's Pizzeria",
+    "https://cdn.pixabay.com/photo/2017/12/10/14/47/piza-3010062_960_720.jpg",
+    "Bon Apatite baebe!!!",
+    "3"
+  ),
+  new Restaurant(
+    "r9",
+    "Allah Wasaiya Tiki burger",
+    "https://cdn.pixabay.com/photo/2014/10/19/20/59/hamburger-494706_960_720.jpg",
+    "Bhaduri k sath",
+    "4"
+  ),
+  new Restaurant(
+    "r10",
+    "Ilyas ilyas ilyas ilyas",
+    "https://cdn.pixabay.com/photo/2014/08/14/14/21/shish-kebab-417994_960_720.jpg",
+    "Akbar, Amjad, Ajmal",
+    "5"
+  ),
+];
+
+export default RESTAURANTS;
